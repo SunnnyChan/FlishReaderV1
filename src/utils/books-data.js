bca027af95591007a0ae2e1de07eced733980671
sunnychan <sunnnychan@gmail.com>
2020-07-19 11:34:35 +0800

    sunnychan

--- src/utils/books-data.js
+++ src/utils/books-data.js
@@ -1,4 +1,4 @@
-import BooksData from '../../data/json/books.json'
+import Insight from '../../data/json/insight.json'
 import Internet from '../../data/json/internet.json'
 import Literature from '../../data/json/literature.json'
 import JavaBooksData from '../../data/json/java.json'
@@ -6,20 +6,24 @@ import EmBooksData from '../../data/json/E-M.json'
 import Programming from '../../data/json/programming.json'
 import GrowthHacking from '../../data/json/growth-hacking.json'
 import DataScience from '../../data/json/data-science.json'
+import DataBase from '../../data/json/db.json'
 import Ai from '../../data/json/ai.json'
+import MathBooks from '../../data/json/math.json'
 
 /**
  * 获取所有的书籍数据（分类）
  */
 export function getBooksData() {
     var books = mergeJson(Literature, EmBooksData)
+    books = mergeJson(Insight, books)
     books = mergeJson(Internet, books)
-    books = mergeJson(JavaBooksData, books)
     books = mergeJson(Ai, books)
+    books = mergeJson(JavaBooksData, books)
+    books = mergeJson(MathBooks, books)
     books = mergeJson(Programming, books)
-    books = mergeJson(GrowthHacking, books)
+    books = mergeJson(DataBase, books)
     books = mergeJson(DataScience, books)
-    return mergeJson(BooksData, books)
+    return mergeJson(GrowthHacking, books)
 }
 
 function mergeJson(jsonbject1, jsonbject2) {
